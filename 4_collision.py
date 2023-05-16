c15a838aa5e531e99fe74dcb0b095129072a0782
dongeal <dongeal@gmail.com>
2023-05-16 09:55:38 +0900

    2:19:50 공쪼개기 전

--- 4_collision.py
+++ 4_collision.py
@@ -76,9 +76,9 @@ balls.append({
     "to_y" : -6, # y 축방향 이동
     "init_spd_y" : ball_speed_y[0]}) # y  최초속도
 
-
-
-
+# 사라질 무기 공 정보 저장 변수
+weapon_to_remove = -1
+ball_to_remove = -1
 
 running = True
 while running:
@@ -153,6 +153,53 @@ while running:
 
     # 4. 충돌처리 
    
+    # 캐릭터 rect 정보 업데이트
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    for ball_idx , ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        # 공 rect 정보 업데이트
+        ball_rect = ball_images[ball_img_idx].get_rect()
+        ball_rect.left = ball_pos_x
+        ball_rect.top = ball_pos_y
+
+        # 공과 캐릭터 충돌 처리
+        if character_rect.colliderect(ball_rect):
+            running = False
+            break
+        
+        # 공과 무기의 충돌 처리
+        for weapon_idx , weapon_val in enumerate(weapons):
+            weapon_pos_x = weapon_val[0]
+            weapon_pos_y = weapon_val[1]
+
+            #무기 rect 정보 없데이트
+            weapon_rect = weapon.get_rect()
+            weapon_rect.left = weapon_pos_x
+            weapon_rect.top = weapon_pos_y
+
+            # 충돌체크
+            if weapon_rect.colliderect(ball_rect):
+                weapon_to_remove = weapon_idx # 해당무기 없애기위한 값설정
+                ball_to_remove = ball_idx #해당 공을 없애기위한 값 설정
+                break
+        
+    #충돌된 공 or 무기 없애기
+    if ball_to_remove > -1:
+        del balls[ball_to_remove]
+        ball_to_remove = -1
+
+    if weapon_to_remove > -1:
+        del weapons[weapon_to_remove]
+        weapon_to_remove = -1
+
+
+
     # 5. 화면에 그리기
 
     screen.blit(background,(0,0))
